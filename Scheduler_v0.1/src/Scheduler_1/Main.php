8928be71d0081b64f9f93e632b0e30c117810a28
mike7140 <mike7140@excite.co.jp>
2016-08-29 10:33:58 +0900

    Add files via upload

---
+++ Scheduler_v0.1/src/Scheduler_1/Main.php
@@ -0,0 +1,28 @@
+<?php
+namespace Scheduler_1;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\scheduler\PluginTask;//これを追加します
+class Main extends PluginBase implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
+	}
+	public function onJoin(PlayerJoinEvent $event){//Playerが参加した時のイベントです
+		$player = $event->getPlayer();
+		$task = new kick($this, $player);//インスタンス作成
+		$this->getServer()->getScheduler()->scheduleDelayedTask($task, 20 * 10);//$playerをkickクラスに20x10tick(10秒後)に継承させます
+	}
+}
+
+class kick extends PluginTask{
+   	public function __construct(PluginBase $owner, Player $player) {
+      		parent::__construct($owner);
+      		$this->player = $player;//Playerデータを引き継ぎます
+   	}
+   	public function onRun($currentTick){
+      		$this->player->kick();//playerをkickします(ちなみにKick by admin.と出ます)
+   	}
+}
